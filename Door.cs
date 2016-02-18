177a8c96bff0b2a8d58f085dafb59ab6b432131b
Benjamin Li <benlibenli@gmail.com>
2016-02-17 18:53:56 -0800

    Created Door class. Yet to be fully implemented

---
+++ Door.cs
@@ -0,0 +1,5 @@
+using System;
+using System.Collections.Generic;   // This is for lists. If you end up not having any lists, you won't need this
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
