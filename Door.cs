fc75d536ad7926021d6c4ef2a1a1d58738f11f02
Benjamin Li <benlibenli@gmail.com>
2016-02-17 19:07:26 -0800

    Implement Door class

--- Door.cs
+++ Door.cs
@@ -3,3 +3,26 @@ using System.Collections.Generic;   // This is for lists. If you end up not havi
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
+namespace Slip
+{
+    public class Door
+    {
+        public static Texture2D texture;
+        public const int size = 20; // Size of the door. Change this if necessary, especially if we want doors of different sizes
+        public Vector2 position;
+        public bool open = false;  // This is False by default, as the door is closed by default
+
+        public void Draw(GameScreen screen, Main main)
+        {
+            main.spriteBatch.Draw(texture, Screen.DrawPos(main, position), null, Color.White, texture.Center());
+        }
+
+        public bool OpenDoor(Player player)
+        {
+            if (Vector2.Distance(player.position, this.position) <= 20f)
+            {
+                this.open = true;
+            }
+        }
+    }
+}
