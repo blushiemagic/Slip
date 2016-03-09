75f99ee14734b011953f5ea90f172aae1cdcaeea
Nathan Kim <bluemagic123@roadrunner.com>
2016-03-09 07:15:27 -0800

    Added level buttons to main menu

--- MainMenu.cs
+++ MainMenu.cs
@@ -9,16 +9,23 @@ namespace Slip
         public override void Initialize(Main main)
         {
             base.Initialize(main);
-            Panel panel = new TextPanel("Testing", Color.Black);
+            Panel panel = new TextPanel("Tutorial", Color.Black);
+            panel.alignment = new Vector2(0.5f, 0.5f);
+            panel.position = new Vector2(0f, -70f);
+            panel.size = new Vector2(300f, 50f);
+            panel.OnClick += (m => m.changeScreen = new Tutorial());
+            AddComponent(panel);
+            panel = new TextPanel("Level 1", Color.Black);
             panel.alignment = new Vector2(0.5f, 0.5f);
             panel.size = new Vector2(300f, 50f);
-            panel.OnClick += StartDemo;
+            panel.OnClick += (m => m.changeScreen = new FirstDungeon());
             AddComponent(panel);
-        }
-
-        private static void StartDemo(Main main)
-        {
-            main.changeScreen = new Tutorial();
+            panel = new TextPanel("Demo", Color.Black);
+            panel.alignment = new Vector2(0.5f, 0.5f);
+            panel.position = new Vector2(0f, 70f);
+            panel.size = new Vector2(300f, 50f);
+            panel.OnClick += (m => m.changeScreen = new DemoScreen());
+            AddComponent(panel);
         }
     }
 }
