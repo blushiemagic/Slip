9f126e65b6dc386dd927c71100b1e4bcb1fa639b
Benjamin Li <benlibenli@gmail.com>
2016-03-09 00:55:01 -0800

    More rooms for FirstDungeon

--- Levels/FirstDungeon.cs
+++ Levels/FirstDungeon.cs
@@ -10,36 +10,61 @@ namespace Slip.Levels
 {
     public class FirstDungeon : GameScreen
     {
-        Room room1 = new Room(18, 14);
-        Room room2 = new Room(5, 5);
+        Room room1 = new Room(18, 7);
+        Room room2 = new Room(18, 18);
+        Room room3 = new Room(20, 20);
+        Room room4 = new Room(5, 5);
 
         public override Room SetupLevel(Player player)
         {
             // Room 1 contents
+            // Note to self: add a door at (4, 3). 
             room1.FillFloor(0, room1.width - 1, 0, room1.height - 1);
             room1.AddWallBorder(0, room1.width - 1, 0, room1.height - 1);
-            
-            for (int x = 1; x < 13; x++)
-            {
-                room1.tiles[x, 5].Wall = 1;
-                room1.tiles[x, 8].Wall = 1;
-            }
-            for (int x = 1; x < 12; x++)
-            {
-                room1.tiles[x, 6].Wall = 1;
-                room1.tiles[x, 7].Wall = 1;
-            }
-            room1.enemies.Add(new Turret(Room.TileToWorldPos(12, 7)));
-            
-            room1.AddPuzzle(1, room1.height - 2, new Portal(room2, Tile.tileSize * new Vector2(1.5f, 1.5f)));
+
+
+            room1.tiles[4, 1].Wall = 1;
+            room1.tiles[4, 2].Wall = 1;
+            room1.tiles[4, 4].Wall = 1;
+            room1.tiles[4, 5].Wall = 1;
+
+            room1.enemies.Add(new Turret(Room.TileToWorldPos(17, 3)));
+            room1.enemies.Add(new Turret(Room.TileToWorldPos(17, 4)));
+            room1.enemies.Add(new Turret(Room.TileToWorldPos(17, 5)));
+            room1.enemies.Add(new Turret(Room.TileToWorldPos(17, 2)));
+
+            room1.AddPuzzle(15, 3, new Portal(room2, Tile.tileSize * new Vector2(1.5f, 1.5f)));
             room1.AddPuzzle(1, 1, new Checkpoint());
 
             // Room 2 contents
+            // Ideally, we would want switches in this room to activate the portal to the next room. 
             room2.FillFloor(0, room2.width - 1, 0, room2.height - 1);
             room2.AddWallBorder(0, room2.width - 1, 0, room2.height - 1);
             
-            room2.AddPuzzle(room2.width - 2, room2.height - 2, new Portal(room1, Tile.tileSize * new Vector2(1.5f, 1.5f)));
+            for (int i = 8; i < 11; i++) {
+                room2.enemies.Add(new Turret(Room.TileToWorldPos(i, 7)));
+                room2.enemies.Add(new Turret(Room.TileToWorldPos(i, 11)));
+                room2.enemies.Add(new Turret(Room.TileToWorldPos(7, i)));
+                room2.enemies.Add(new Turret(Room.TileToWorldPos(11, i)));
+            }
 
+            room2.AddPuzzle(room2.width - 2, room2.height - 2, new Portal(room3, Tile.tileSize * new Vector2(1.5f, 1.5f)));
+            room2.AddPuzzle(1, 1, new Checkpoint());
+
+            // Room 3 contents
+            // The room will have some enemies to fight. Ideally, when all the enemies are dead, the portal will open up. 
+            room3.FillFloor(0, room3.width - 1, 0, room3.height - 1);
+            room3.AddWallBorder(0, room3.width - 1, 0, room3.height - 1);
+
+            room3.AddPuzzle(room3.width - 2, room3.height - 2, new Portal(room4, Tile.tileSize * new Vector2(1.5f, 1.5f)));
+            room3.AddPuzzle(1, 1, new Checkpoint());
+
+            // Room 4 contents
+            room4.FillFloor(0, room4.width - 1, 0, room4.height - 1);
+            room4.AddWallBorder(0, room4.width - 1, 0, room4.height - 1);
+
+
+            // Returns the first room to be loaded
             return room1;
         }
 
